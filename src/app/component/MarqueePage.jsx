e6255f20fbb7c96970093c60e57d0668110adc4b
Muhammad Fahad Jamal <muhammadfahadbinjamal@gamil.com>
2026-05-02 12:24:15 +0600

    Marquee page add

---
+++ src/app/component/MarqueePage.jsx
@@ -0,0 +1,26 @@
+
+import Marquee from "react-fast-marquee";
+
+const MarqueePage = () => {
+  return (
+    <div className="mt-5 bg-amber-50 py-2">
+      <Marquee pauseOnHover={true} speed={50}>
+        <p className="font-bold text-xl text-amber-900 mx-6">
+          New Arrivals
+        </p>
+        <p className="font-bold text-xl text-amber-900 mx-6">|</p>
+
+        <p className="font-bold text-xl text-amber-900 mx-6">
+          Weekly Feature: Modern Geometric Patterns
+        </p>
+        <p className="font-bold text-xl text-amber-900 mx-6">|</p>
+
+        <p className="font-bold text-xl text-amber-900 mx-6">
+          Join Our Community
+        </p>
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Banner from "./component/Banner";
 import FeaturedTiles from "./component/FeaturedTiles";
-// import MarqueePage from "./component/MarqueePage";
+import MarqueePage from "./component/MarqueePage";
 
 export default function Home() {
   return (
    <div className="max-w-7xl mx-auto">
-    {/* <MarqueePage/> */}
+    <MarqueePage/>
     <Banner/>
     <FeaturedTiles/>
    </div>
